64b0912824d0cca0a36de961c328d9245909ac7e
Josue Torres <jtn1999@estudiantec.cr>
2025-09-13 16:51:07 -0600

    1.14

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
+
 import createNextIntlPlugin from "next-intl/plugin";
+
+
 
 const withNextIntl = createNextIntlPlugin();
 
+
+
 const nextConfig: NextConfig = {
-  // Add the output property here
-  output: 'export',
-  // your other config options
+
+  /* config options here */
 };
 
-export default withNextIntl(nextConfig)
+
+
+export default withNextIntl(nextConfig);
